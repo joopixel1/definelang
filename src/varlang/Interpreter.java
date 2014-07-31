c4ee5af894d05743e97d7801736e4535c829097c
Hridesh Rajan <hridesh@cs.iastate.edu>
2014-07-31 03:08:05 -0500

    recover from LookupException and continue.

--- src/varlang/Interpreter.java
+++ src/varlang/Interpreter.java
@@ -13,7 +13,7 @@ import varlang.AST.*;
 public class Interpreter {
 	public static void main(String[] args) {
 		System.out.println("Type a program to evaluate and press the enter key," + 
-							" e.g. (+ (* 3 100) (/ 84 (- 279 277))) \n" + 
+							" e.g. (let ((a 3) (b 100) (c 84) (d 279) (e 277)) (+ (* a b) (/ c (- d e)))) \n" + 
 							"Press Ctrl + C to exit.");
 		Reader reader = new Reader();
 		Evaluator eval = new Evaluator();
@@ -21,8 +21,12 @@ public class Interpreter {
 		try {
 			while (true) { // Read-Eval-Print-Loop (also known as REPL)
 				Program p = reader.read();
-				Value val = eval.valueOf(p);
-				printer.print(val);
+				try {
+					Value val = eval.valueOf(p);
+					printer.print(val);
+				} catch (Env.LookupException e) {
+					printer.print(e);
+				}
 			}
 		} catch (IOException e) {
 			System.out.println("Error reading input.");
