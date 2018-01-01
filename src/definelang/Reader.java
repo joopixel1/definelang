05d06e79433a515b9cb4e4b5ccac7b010c2d8c5a
Hridesh Rajan <hridesh@gmail.com>
2018-01-01 13:46:06 -0600

    fix system dependency

--- src/definelang/Reader.java
+++ src/definelang/Reader.java
@@ -1,6 +1,7 @@
 package definelang;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
@@ -37,7 +38,7 @@ public class Reader {
 		return runFile(programText);
 	}
 
-	protected String getProgramDirectory() { return "build/definelang/examples/"; }
+	protected String getProgramDirectory() { return "build"+File.separator+"definelang"+File.separator+"examples"+File.separator; }
 	protected String runFile(String programText) throws IOException {
 		if(programText.startsWith("run ")){
 			programText = readFile(getProgramDirectory() + programText.substring(4));
